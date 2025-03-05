ef49453075513a7b7410fbab00d7a02e5397166e
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2025-03-05 03:29:23 +0100

    render adgang

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 10000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 10000
+  }
 })
+
